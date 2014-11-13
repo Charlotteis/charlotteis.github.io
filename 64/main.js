21fa23262c0169c46f4424f519477aace75944a9
Charlotte Spencer <charlottelaspencer@gmail.com>
2014-11-13 18:35:28 +0000

    Added click interactivity

--- 64/main.js
+++ 64/main.js
@@ -9,3 +9,9 @@ heading.addEventListener("mouseleave", function(event) {
     event.target.innerHTML = "8<sup>8</sup>";
     console.log("done!");
 });
+
+var click = document.getElementById("click");
+
+click.addEventListener("click", function(event) {
+    this.innerHTML += "<br><span class='eight'>1 2 3 4 5 6 7 8</span>"
+})
